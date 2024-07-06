df931866b927e73f08c2937c245b09977600c90e
Robert Toth-Ludanyi <roberttothmails@yahoo.com>
2024-07-06 14:46:33 +0200

    Basic setup done

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,33 @@
 // Write your "actions" router here!
+const express = require('express');
+const Action = require('./actions-model');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    
+  });
+  
+  router.get('/:id', (req, res, next) => {
+    
+  });
+  
+  router.post('/', (req, res, next) => {
+    
+  });
+  
+  router.put('/:id', (req, res, next) => {
+    
+  });
+  
+  router.delete('/:id', (req, res, next) => {
+   
+  });
+  
+  router.use((error, req, res, next) => { //eslint-disable-line
+    res.status(error.status || 500).json({
+      message: error.message,
+      customMessage: 'Something bad happened inside the users-router',
+      stack: error.stack,
+    });
+  });
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,38 @@
 // Write your "projects" router here!
+const express = require('express');
+const Projects = require('./projects-model');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+
+});
+
+router.get('/:id', (req, res, next) => {
+
+});
+
+router.post('/', (req, res, next) => {
+
+});
+
+router.put('/:id', (req, res, next) => {
+
+});
+
+router.delete('/:id', (req, res, next) => {
+
+});
+
+router.get('/:id/actions', (req, res, next) => {
+
+});
+
+
+//   router.use((error, req, res, next) => { //eslint-disable-line
+//     res.status(error.status || 500).json({
+//       message: error.message,
+//       customMessage: 'Something bad happened inside the users-router',
+//       stack: error.stack,
+//     });
+//   });
