bf57b8e30f3ca4594b2039cad8941748543c61f7
Robert Toth-Ludanyi <roberttothmails@yahoo.com>
2024-07-08 11:48:43 +0200

    Projects router done

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,20 +1,31 @@
 const Projects = require('./projects-model');
 
-async function validateAction(req, res, next) {
-    const { id } = req.params;
+async function validateProjectId(req, res, next) {
+    const { id } = req.params
     try {
-        const actions = await Projects.getProjectActions(id);
-        if (actions.length > 0) {
-            req.action = actions;
-            next();
+        const project = await Projects.get(id)
+        if (project) {
+            req.project = project
+            next()
         } else {
-            res.status(404).json({ message: 'Actions not found' });
+            res.status(404).json({
+                message: `Project not found`
+            })
         }
     } catch (err) {
-        res.status(500).json({ message: 'Fatal server error' });
+        res.status(500).json({ message: 'Error retrieving project' });
     }
 }
 
+function validateProject(req, res, next) {
+    const { name, description, completed } = req.body;
+    if (!name || !description || typeof completed === 'undefined' ) {
+        return res.status(400).json({ message: 'Name, discription and completed is needed' });
+    }
+    next()
+}
+
 module.exports = {
-    validateAction,
+    validateProjectId,
+    validateProject,
 };
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,43 +1,70 @@
 // Write your "projects" router here!
 const express = require('express');
 const Projects = require('./projects-model');
-const { validateAction } = require('./projects-middleware');
+const {  
+  validateProjectId, 
+  validateProject 
+} = require('./projects-middleware');
 const router = express.Router();
 
 
 
-router.get('/', (req, res, next) => {
-
+router.get('/', async (req, res, next) => {
+  try {
+    const projects = await Projects.get();
+    res.json(projects);
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.get('/:id', (req, res, next) => {
-
+router.get('/:id', validateProjectId, (req, res, next) => {
+  Projects.get(req.params.id)
+    .then(project => {
+      res.status(200).json(project)
+    })
+    .catch(next)
 });
 
-router.post('/', (req, res, next) => {
-
+router.post('/', validateProject, (req, res, next) => {
+  Projects.insert(req.body)
+    .then(project => {
+      res.status(201).json(project)
+    })
+    .catch(next)
 });
 
-router.put('/:id', (req, res, next) => {
-
+router.put('/:id',validateProjectId, validateProject, (req, res, next) => {
+  Projects.update(req.params.id, req.body)
+    .then(project => {
+      res.status(200).json(project)
+    })
+    .catch(next)
 });
 
-router.delete('/:id', (req, res, next) => {
-
+router.delete('/:id', validateProjectId, (req, res, next) => {
+  Projects.remove(req.params.id)
+    .then(() => {
+      res.json(req.projects)
+    })
+    .catch(next)
 });
 
-router.get('/:id/actions', validateAction, (req, res, next) => {
-  console.log(req.action);
-  res.json(req.action);
+router.get('/:id/actions', validateProjectId, async (req, res, next) => {
+  try {
+    const actions = await Projects.getProjectActions(req.params.id);
+    res.status(200).json(actions);
+} catch (err) {
+    next(err);
+}
 });
 
-
-// router.use((error, req, res, next) => { //eslint-disable-line
-//   res.status(error.status || 500).json({
-//     message: error.message,
-//     customMessage: 'Something bad happened inside the users-router',
-//     stack: error.stack,
-//   });
-// });1
+router.use((error, req, res, next) => { //eslint-disable-line
+  res.status(error.status || 500).json({
+    message: error.message,
+    customMessage: 'Something bad happened inside the projects-router',
+    stack: error.stack,
+  });
+});
 
 module.exports = router
